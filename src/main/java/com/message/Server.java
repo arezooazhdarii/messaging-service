d759989a703f0bbbff7c7181ac511f10c568d956
arezooazhdari <arezooazhdarii@gmail.co>
2023-11-15 11:37:52 +0400

    Initial commit

---
+++ src/main/java/com/message/Server.java
@@ -0,0 +1,45 @@
+package com.message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        String clientUsername = null;
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket,clientUsername);
+                System.out.println("a new client has connected!" + clientHandler.clientUsername);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8090);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
+}
